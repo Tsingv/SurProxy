fb95d6c452fab2bc3dccbbedd9fbab63dcaa6d19
Tsingv <34980893+Tsingv@users.noreply.github.com>
2026-03-24 20:41:08 +0800

    Initial Commit

---
+++ SurProxy/SurProxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurProxyApp.swift
+//  SurProxy
+//
+//  Created by clearain on 2026/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SurProxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
